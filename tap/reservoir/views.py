f47527c6a22c333d820ca2d5ccc754320550149d
Andrew Van <vanandrew@wustl.edu>
2019-05-21 19:30:28 -0500

    start on abcd db

--- tap/reservoir/views.py
+++ tap/reservoir/views.py
@@ -1,3 +1,15 @@
+import os
+from bids import BIDSLayout
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    # set root abcd directory
+    # TODO: do not hard code this in the future
+    abcd_root = '/mnt/Daenerys/ABCD/data'
+
+    # get bids layout
+    layout = BIDSLayout(os.path.join(abcd_root,'abcdbids_input'))
+
+    return HttpResponse('Test')
