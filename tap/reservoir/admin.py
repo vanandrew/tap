7a85daf721f4cb7009b8c0a3d1e23f0fe357f796
Andrew Van <vanandrew@wustl.edu>
2019-05-26 00:41:02 -0500

    start on db models

--- tap/reservoir/admin.py
+++ tap/reservoir/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class SubjectAdmin(admin.ModelAdmin):
+    pass
+
+class SessionAdmin(admin.ModelAdmin):
+    pass
+
+class AnatAdmin(admin.ModelAdmin):
+    pass
+
+class FmapAdmin(admin.ModelAdmin):
+    pass
+
+class FuncAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Subject,SubjectAdmin)
+admin.site.register(Session,SessionAdmin)
+admin.site.register(Anat,AnatAdmin)
+admin.site.register(Fmap,FmapAdmin)
+admin.site.register(Func,FuncAdmin)
+
+admin.site.site_title = 'tap Admin'
+admin.site.site_header = 'tap Admin'
+admin.site.index_title = 'tap Admin Home'
--- tap/reservoir/models.py
+++ tap/reservoir/models.py
@@ -1,3 +1,89 @@
 from django.db import models
 
-# Create your models here.
+# Define base BIDS data model used by session, but also subject if no session is defined
+class BaseBIDSDataClass(models.Model):
+    path = models.TextField(
+        "Path"
+    )
+
+    class Meta:
+        abstract = True
+
+class Subject(BaseBIDSDataClass):
+    subject = models.CharField(
+        "Subject",
+        max_length = 255,
+        primary_key=True
+    )
+    
+    class Meta:
+        verbose_name = "Subject"
+
+class Session(BaseBIDSDataClass):
+    session = models.CharField(
+        "Session",
+        max_length=255,
+    )
+ 
+    subject = models.ForeignKey(
+        "Subject",
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name = "Session"
+
+# Define a base BIDS File model
+class BIDSFile(models.Model):
+    filename = models.CharField(
+        "Filename",
+        max_length=255,
+        primary_key=True
+    )
+
+    sidecar = models.CharField(
+        "JSON Sidecar",   
+        max_length=255
+    )
+
+    subject = models.ForeignKey(
+        Subject,
+        on_delete=models.CASCADE,
+    )
+
+    session = models.ForeignKey(
+        Session,
+        on_delete=models.CASCADE,
+    )
+
+    path = models.TextField(
+        "Path"
+    )
+
+    run = models.IntegerField(
+        "Run",
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        abstract = True
+
+class Anat(BIDSFile):
+    class Meta:
+        verbose_name = "Anat"
+
+class Fmap(BIDSFile):
+    class Meta:
+        verbose_name = "Fmap"
+
+class Func(BIDSFile):
+    task = models.CharField(
+        "Task",
+        max_length=255,
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = "Func"
