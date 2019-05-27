f775ffe7a1c2f171ce00ff49f266b1dee9d0cc83
Andrew Van <vanandrew@wustl.edu>
2019-05-27 00:54:50 -0500

    update models with more metadata fields

--- tap/reservoir/models.py
+++ tap/reservoir/models.py
@@ -1,4 +1,7 @@
+import os
+import json
 from django.db import models
+from django.conf import settings
 
 # Define base BIDS data model used by session, but also subject if no session is defined
 class BaseBIDSDataClass(models.Model):
@@ -15,9 +18,12 @@ class Subject(BaseBIDSDataClass):
         max_length = 255,
         primary_key=True
     )
-    
+ 
     class Meta:
         verbose_name = "Subject"
+
+    def __str__(self):
+        return self.subject
 
 class Session(BaseBIDSDataClass):
     session = models.CharField(
@@ -33,6 +39,9 @@ class Session(BaseBIDSDataClass):
     class Meta:
         verbose_name = "Session"
 
+    def __str__(self):
+        return self.session
+
 # Define a base BIDS File model
 class BIDSFile(models.Model):
     filename = models.CharField(
@@ -42,8 +51,10 @@ class BIDSFile(models.Model):
     )
 
     sidecar = models.CharField(
-        "JSON Sidecar",   
+        "JSON Sidecar", 
         max_length=255
+        blank=True,
+        null=True
     )
 
     subject = models.ForeignKey(
@@ -54,6 +65,8 @@ class BIDSFile(models.Model):
     session = models.ForeignKey(
         Session,
         on_delete=models.CASCADE,
+        blank=True,
+        null=True
     )
 
     path = models.TextField(
@@ -69,15 +82,71 @@ class BIDSFile(models.Model):
     class Meta:
         abstract = True
 
+    def __str__(self):
+        return self.filename
+
+def createmetafield(metafields,key):
+    # detect field type
+    metatype = type(metafields[key])
+ 
+    # create int field
+    if metatype == int:
+        return models.IntegerField(
+            key,
+            blank=True,
+            null=True
+        )
+    elif metatype == float:
+        return models.FloatField(
+            key,
+            blank=True,
+            null=True
+        )
+    # create character field
+    elif metatype == str or metatype == list:
+        return models.CharField(
+            key,
+            max_length=255,
+            blank=True,
+            null=True
+        )
+    # raise validation error if no match
+    else:
+        raise ValueError("Could not find matching type for {}. Check BIDSMetaTemplates.".format(metatype))
+
 class Anat(BIDSFile):
+    # load template file
+    with open(os.path.join(settings.BASE_DIR,'reservoir/BIDSMetaTemplates/anat.json'),'r') as metafile:
+        metafields = json.load(metafile)
+
+    # create fields
+    for key in metafields:
+        vars()[key] = createmetafield(metafields,key) 
+
     class Meta:
         verbose_name = "Anat"
 
 class Fmap(BIDSFile):
+    # load template file
+    with open(os.path.join(settings.BASE_DIR,'reservoir/BIDSMetaTemplates/fmap.json'),'r') as metafile:
+        metafields = json.load(metafile)
+
+    # create fields
+    for key in metafields:
+        vars()[key] = createmetafield(metafields,key)
+
     class Meta:
         verbose_name = "Fmap"
 
 class Func(BIDSFile):
+    # load template file
+    with open(os.path.join(settings.BASE_DIR,'reservoir/BIDSMetaTemplates/func.json'),'r') as metafile:
+        metafields = json.load(metafile)
+
+    # create fields
+    for key in metafields:
+        vars()[key] = createmetafield(metafields,key)
+
     task = models.CharField(
         "Task",
         max_length=255,
