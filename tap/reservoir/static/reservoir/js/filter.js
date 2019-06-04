e3d646755ab55f87fbea843969acef2fc3cb4b96
Andrew Van <vanandrew@wustl.edu>
2019-06-04 03:43:32 -0500

    filter component

--- tap/reservoir/static/reservoir/js/filter.js
+++ tap/reservoir/static/reservoir/js/filter.js
@@ -1,8 +1,10 @@
 "use strict";
 
-// Create Options for filter
+// Helper Functions
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -10,29 +12,169 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+function getObjectKey(obj) {
+  return Object.keys(obj)[0];
+}
+function addtoArray(array, value) {
+  array.push(value);return array;
+}
+function removefromArray(array, value) {
+  return array.filter(function (el) {
+    var el_key = getObjectKey(el);
+    var value_key = getObjectKey(value);
+    if (el_key === value_key) {
+      if (el[el_key] === value[value_key]) {
+        return false;
+      }
+    }
+    return true;
+  });
+}
+function inArray(array, value) {
+  for (var i = 0; i < array.length; i++) {
+    var array_key = getObjectKey(array[i]);
+    var value_key = getObjectKey(value);
+    if (array_key === value_key) {
+      if (array[i][array_key] === value[value_key]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// Create Options for filter
+
 var Options = function (_React$Component) {
   _inherits(Options, _React$Component);
 
   function Options(props) {
     _classCallCheck(this, Options);
 
-    return _possibleConstructorReturn(this, (Options.__proto__ || Object.getPrototypeOf(Options)).call(this, props));
+    // bind methods
+    var _this = _possibleConstructorReturn(this, (Options.__proto__ || Object.getPrototypeOf(Options)).call(this, props));
+
+    _this.selectClass = _this.selectClass.bind(_this);
+    _this.updateFilters = _this.updateFilters.bind(_this);
+    return _this;
   }
 
+  // choose class based on selected option
+
+
   _createClass(Options, [{
+    key: "selectClass",
+    value: function selectClass(option) {
+      // Declare constants
+      var inactive = "badge-secondary";
+      var include = "badge-success";
+      var exclude = "badge-danger";
+
+      // Choose class to display
+      if (inArray(this.props.filters.include, option)) {
+        return include;
+      } else if (inArray(this.props.filters.exclude, option)) {
+        return exclude;
+      } else {
+        return inactive;
+      }
+    }
+
+    // update the filter selection
+
+  }, {
+    key: "updateFilters",
+    value: function updateFilters(event) {
+      this.props.updateFilters(_defineProperty({}, this.props.field, event.target.getAttribute('value')));
+    }
+
+    // Render Options
+
+  }, {
     key: "render",
     value: function render() {
-      return React.createElement(
-        "h4",
-        null,
-        this.props.value.map(function (o) {
-          return React.createElement(
-            "span",
-            { className: "badge badge-secondary mx-1", key: o },
-            o
-          );
-        })
-      );
+      var _this2 = this;
+
+      try {
+        return React.createElement(
+          "div",
+          { className: "container" },
+          React.createElement(
+            "div",
+            { className: "jumbotron my-1 px-2 py-4" },
+            React.createElement(
+              "h6",
+              { className: "mx-1" },
+              "Available Values:"
+            ),
+            React.createElement(
+              "p",
+              null,
+              this.props.value.map(function (o) {
+                return React.createElement(
+                  "span",
+                  { onClick: _this2.updateFilters,
+                    style: {
+                      cursor: "pointer",
+                      WebkitTouchCallout: "none",
+                      WebkitUserSelect: "none",
+                      MozUserSelect: "none",
+                      msUserSelect: "none",
+                      userSelect: "none" },
+                    className: "badge " + _this2.selectClass(_defineProperty({}, _this2.props.field, o)) + " mx-1",
+                    value: o, key: o },
+                  o
+                );
+              })
+            )
+          ),
+          React.createElement(
+            "div",
+            { className: "jumbotron my-1 px-2 py-4" },
+            React.createElement(
+              "h6",
+              { className: "mx-1" },
+              "Include:"
+            ),
+            React.createElement(
+              "p",
+              null,
+              this.props.filters.include.map(function (i) {
+                return React.createElement(
+                  "span",
+                  { className: "badge badge-success mx-1",
+                    key: "include_" + i[getObjectKey(i)] },
+                  i[getObjectKey(i)]
+                );
+              })
+            )
+          ),
+          React.createElement(
+            "div",
+            { className: "jumbotron my-1 px-2 py-4" },
+            React.createElement(
+              "h6",
+              { className: "mx-1" },
+              "Exclude:"
+            ),
+            React.createElement(
+              "p",
+              null,
+              this.props.filters.exclude.map(function (e) {
+                return React.createElement(
+                  "span",
+                  { className: "badge badge-danger mx-1",
+                    key: "exclude_" + e[getObjectKey(e)] },
+                  e[getObjectKey(e)]
+                );
+              })
+            )
+          )
+        );
+      } catch (error) {
+        console.log(error);
+        return null;
+      }
     }
   }]);
 
@@ -49,18 +191,21 @@ var Filter = function (_React$Component2) {
     _classCallCheck(this, Filter);
 
     // initialize state
-    var _this2 = _possibleConstructorReturn(this, (Filter.__proto__ || Object.getPrototypeOf(Filter)).call(this, props));
+    var _this3 = _possibleConstructorReturn(this, (Filter.__proto__ || Object.getPrototypeOf(Filter)).call(this, props));
 
-    _this2.state = {
+    _this3.state = {
       fields: null,
       isLoaded: false,
       selection: null,
-      options: null
+      options: null,
+      include: [],
+      exclude: []
 
       // bind methods
-    };_this2.handleChange = _this2.handleChange.bind(_this2);
-    _this2.getOptions = _this2.getOptions.bind(_this2);
-    return _this2;
+    };_this3.handleChange = _this3.handleChange.bind(_this3);
+    _this3.getOptions = _this3.getOptions.bind(_this3);
+    _this3.updateFilters = _this3.updateFilters.bind(_this3);
+    return _this3;
   }
 
   // Execute on creation of component
@@ -69,16 +214,16 @@ var Filter = function (_React$Component2) {
   _createClass(Filter, [{
     key: "componentDidMount",
     value: function componentDidMount() {
-      var _this3 = this;
+      var _this4 = this;
 
       // grab the available fields
       $.get(this.props.api_fields, function (data) {
-        _this3.setState({
+        _this4.setState({
           fields: data.fields,
           selection: 'datatype'
         }, function () {
-          return _this3.getOptions(function () {
-            return _this3.setState({ isLoaded: true });
+          return _this4.getOptions(function () {
+            return _this4.setState({ isLoaded: true });
           });
         });
       });
@@ -89,11 +234,10 @@ var Filter = function (_React$Component2) {
   }, {
     key: "getOptions",
     value: function getOptions(callback) {
-      var _this4 = this;
+      var _this5 = this;
 
-      console.log(this.state.selection);
       $.get(this.props.api_unique + "/" + this.state.selection, function (data) {
-        _this4.setState({ options: data[_this4.state.selection] }, callback);
+        _this5.setState({ options: data[_this5.state.selection] }, callback);
       });
     }
 
@@ -102,11 +246,42 @@ var Filter = function (_React$Component2) {
   }, {
     key: "handleChange",
     value: function handleChange(event) {
-      var _this5 = this;
+      var _this6 = this;
 
       this.setState({ selection: event.target.value }, function () {
-        return _this5.getOptions();
+        return _this6.getOptions();
       });
+    }
+
+    // update filters
+
+  }, {
+    key: "updateFilters",
+    value: function updateFilters(option) {
+      // cycle through inactive/include/exclude
+      function cycleOptions(state, value) {
+        if (!inArray(state.include, value) && !inArray(state.exclude, value)) {
+          return {
+            include: addtoArray(Array.from(state.include), value),
+            exclude: state.exclude
+          };
+        } else if (inArray(state.include, value) && !inArray(state.exclude, value)) {
+          return {
+            include: removefromArray(Array.from(state.include), value),
+            exclude: addtoArray(Array.from(state.exclude), value)
+          };
+        } else if (!inArray(state.include, value) && inArray(state.exclude, value)) {
+          return {
+            include: state.include,
+            exclude: removefromArray(Array.from(state.exclude), value)
+          };
+        } else {
+          console.log('We\'re not suppose to be here!');
+        }
+      }
+
+      // Set state for filters
+      this.setState(cycleOptions(this.state, option));
     }
 
     // render component
@@ -117,7 +292,7 @@ var Filter = function (_React$Component2) {
       if (this.state.isLoaded) {
         return React.createElement(
           "div",
-          { className: "offset-1 col-10 mt-4 mb-1" },
+          { className: "offset-1 col-10 mt-4 mb-2" },
           React.createElement(
             "div",
             { className: "accordian" },
@@ -129,7 +304,8 @@ var Filter = function (_React$Component2) {
                 { className: "card-header" },
                 React.createElement(
                   "button",
-                  { className: "btn btn-link", "data-toggle": "collapse", "data-target": "#filtercard" },
+                  { className: "btn btn-link", "data-toggle": "collapse",
+                    "data-target": "#filtercard" },
                   "Filters"
                 )
               ),
@@ -152,7 +328,8 @@ var Filter = function (_React$Component2) {
                       ),
                       React.createElement(
                         "select",
-                        { value: this.state.selection, onChange: this.handleChange, className: "form-control", id: "fieldselector" },
+                        { value: this.state.selection, onChange: this.handleChange,
+                          className: "form-control", id: "fieldselector" },
                         this.state.fields.map(function (f) {
                           return React.createElement(
                             "option",
@@ -163,7 +340,9 @@ var Filter = function (_React$Component2) {
                       )
                     )
                   ),
-                  React.createElement(Options, { value: this.state.options })
+                  React.createElement(Options, { field: this.state.selection, value: this.state.options,
+                    filters: { include: this.state.include, exclude: this.state.exclude },
+                    updateFilters: this.updateFilters })
                 )
               )
             )
--- tap/reservoir/static/reservoir/js/src/filter.jsx
+++ tap/reservoir/static/reservoir/js/src/filter.jsx
@@ -1,19 +1,115 @@
 "use strict";
 
+// Helper Functions
+function getObjectKey(obj) { return Object.keys(obj)[0] }
+function addtoArray(array,value) { array.push(value); return array; }
+function removefromArray(array,value) {
+  return array.filter((el) => {
+      var el_key = getObjectKey(el);
+      var value_key = getObjectKey(value);
+      if (el_key === value_key) {
+        if (el[el_key] === value[value_key]) {
+          return false;
+        }
+      }
+      return true;
+    }
+  );
+}
+function inArray(array,value) {
+  for (var i = 0; i < array.length; i++) {
+    var array_key = getObjectKey(array[i]);
+    var value_key = getObjectKey(value);
+    if (array_key === value_key) {
+      if (array[i][array_key] === value[value_key]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 // Create Options for filter
 class Options extends React.Component{
   constructor(props) {
     super(props);
+
+    // bind methods
+    this.selectClass = this.selectClass.bind(this);
+    this.updateFilters = this.updateFilters.bind(this);
   }
 
+  // choose class based on selected option
+  selectClass(option) {
+    // Declare constants
+    const inactive = "badge-secondary";
+    const include = "badge-success";
+    const exclude = "badge-danger";
+
+    // Choose class to display
+    if ( inArray(this.props.filters.include,option) ) {
+      return include;
+    }
+    else if ( inArray(this.props.filters.exclude,option) ) {
+      return exclude;
+    }
+    else {
+      return inactive;
+    }
+  }
+
+  // update the filter selection
+  updateFilters(event) {
+    this.props.updateFilters({[this.props.field]: event.target.getAttribute('value')});
+  }
+
+  // Render Options
   render() {
-    return (
-      <h4>
-        {this.props.value.map((o)=>(
-          <span className="badge badge-secondary mx-1" key={o}>{o}</span>
-        ))}
-      </h4>
-    );
+    try {
+      return (
+        <div className="container">
+          <div className="jumbotron my-1 px-2 py-4">
+            <h6 className="mx-1">Available Values:</h6>
+            <p>
+              {this.props.value.map((o)=>(
+                <span onClick={this.updateFilters}
+                  style={{
+                    cursor: "pointer",
+                    WebkitTouchCallout: "none",
+                    WebkitUserSelect: "none",
+                    MozUserSelect: "none",
+                    msUserSelect: "none",
+                    userSelect: "none"}}
+                  className={`badge ${this.selectClass({[this.props.field]: o})} mx-1`}
+                  value={o} key={o}>{o}</span>
+              ))}
+            </p>
+          </div>
+          <div className="jumbotron my-1 px-2 py-4">
+            <h6 className="mx-1">Include:</h6>
+            <p>
+              {this.props.filters.include.map((i)=>(
+                <span className="badge badge-success mx-1"
+                  key={`include_${i[getObjectKey(i)]}`}>{i[getObjectKey(i)]}</span>
+              ))}
+            </p>
+          </div>
+          <div className="jumbotron my-1 px-2 py-4">
+            <h6 className="mx-1">Exclude:</h6>
+            <p>
+              {this.props.filters.exclude.map((e)=>(
+                <span className="badge badge-danger mx-1"
+                  key={`exclude_${e[getObjectKey(e)]}`}>{e[getObjectKey(e)]}</span>
+              ))}
+            </p>
+          </div>
+        </div>
+      );
+    }
+    catch (error) {
+      console.log(error);
+      return null;
+    }
   }
 }
 
@@ -27,12 +123,15 @@ class Filter extends React.Component{
       fields: null,
       isLoaded: false,
       selection: null,
-      options: null
+      options: null,
+      include: [],
+      exclude: []
     }
 
     // bind methods
     this.handleChange = this.handleChange.bind(this);
     this.getOptions = this.getOptions.bind(this);
+    this.updateFilters = this.updateFilters.bind(this);
   }
 
   // Execute on creation of component
@@ -41,14 +140,13 @@ class Filter extends React.Component{
     $.get(this.props.api_fields, (data) => {
       this.setState({
         fields: data.fields,
-        selection: 'datatype'
+        selection: 'datatype',
       }, () => this.getOptions( () => this.setState({isLoaded: true}) ) );
     });
   }
 
   // load the uniques
   getOptions(callback) {
-    console.log(this.state.selection)
     $.get(`${this.props.api_unique}/${this.state.selection}`, (data) => {
       this.setState({options: data[this.state.selection]}, callback);
     });
@@ -59,29 +157,64 @@ class Filter extends React.Component{
     this.setState({selection: event.target.value}, () => this.getOptions() );
   }
 
+  // update filters
+  updateFilters(option) {
+    // cycle through inactive/include/exclude
+    function cycleOptions(state,value) {
+      if ( !inArray(state.include,value) && !inArray(state.exclude,value) ) {
+        return {
+          include: addtoArray(Array.from(state.include),value),
+          exclude: state.exclude
+        }
+      }
+      else if ( inArray(state.include,value) && !inArray(state.exclude,value) ) {
+        return {
+          include: removefromArray(Array.from(state.include),value),
+          exclude: addtoArray(Array.from(state.exclude),value)
+        }
+      }
+      else if ( !inArray(state.include,value) && inArray(state.exclude,value) )  {
+        return {
+          include: state.include,
+          exclude: removefromArray(Array.from(state.exclude),value)
+        }
+      }
+      else {
+        console.log('We\'re not suppose to be here!')
+      }
+    }
+
+    // Set state for filters
+    this.setState(cycleOptions(this.state,option))
+  }
+
   // render component
   render() {
     if (this.state.isLoaded) {
       return (
-        <div className="offset-1 col-10 mt-4 mb-1">
+        <div className="offset-1 col-10 mt-4 mb-2">
           <div className="accordian">
             <div className="card">
               <div className="card-header">
-                <button className="btn btn-link" data-toggle="collapse" data-target="#filtercard">Filters</button>
+                <button className="btn btn-link" data-toggle="collapse"
+                  data-target="#filtercard">Filters</button>
               </div>
               <div className="collapse" id='filtercard'>
                 <div className="card-body">
                   <form>
                     <div className="form-group">
                       <label htmlFor="fieldselector">Field Select</label>
-                      <select value={this.state.selection} onChange={this.handleChange} className="form-control" id="fieldselector">
+                      <select value={this.state.selection} onChange={this.handleChange}
+                        className="form-control" id="fieldselector">
                         {this.state.fields.map((f)=>(
                           <option key={f} value={f}>{f}</option>
                         ))}
                       </select>
                     </div>
                   </form>
-                  <Options value={this.state.options} />
+                  <Options field={this.state.selection} value={this.state.options}
+                    filters={{include: this.state.include, exclude: this.state.exclude}}
+                    updateFilters={this.updateFilters} />
                 </div>
               </div>
             </div>
